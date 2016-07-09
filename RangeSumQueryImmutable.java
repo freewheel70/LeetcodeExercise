bfc8e1695a7f87b742ef2a68ab1adbeaabe056cc
freewheel70 <lizhihong370@gmail.com>
2016-07-10 00:18:32 +0800

    RangeSumQueryImmutable

--- RangeSumQueryImmutable.java
+++ RangeSumQueryImmutable.java
@@ -1,44 +1,63 @@
 package leetcode;
 
+import javafx.util.Pair;
+
 import java.util.HashMap;
 
 /**
  * Created by Administrator on 2016/7/9.
  */
 public class RangeSumQueryImmutable {
+
     static class NumArray {
+        private int[] sum;
+
+        public NumArray(int[] nums) {
+            sum = new int[nums.length + 1];
+            for (int i = 0; i < nums.length; i++) {
+                sum[i + 1] = sum[i] + nums[i];
+            }
+        }
+
+        public int sumRange(int i, int j) {
+            return sum[j + 1] - sum[i];
+        }
+    }
+
+    static class NumArray2 {
 
         HashMap<Marks, Integer> cache = new HashMap<>();
 
         int[] nums = null;
 
-        public NumArray(int[] nums) {
+        public NumArray2(int[] nums) {
             this.nums = nums;
+            if (nums.length > 0) {
+                cache.put(new Marks(0, 0), nums[0]);
+
+                for (int i = 1; i < nums.length; i++) {
+                    int result = nums[i] + cache.get(new Marks(0, i - 1));
+                    cache.put(new Marks(0, i), result);
+                }
+            }
         }
 
         public int sumRange(int i, int j) {
-            int result = 0;
-            if (i == j) {
-                return nums[i];
-            } else {
-                Marks currentMark = new Marks(i, j);
-                Integer value = cache.get(currentMark);
-                if (value != null) {
-                    return value;
-                } else {
-                    if (j - i > 10) {
-                        for (int k = 0; k < j - i - 10; k++) {
-                            result += nums[i + k];
-                        }
-                        result += sumRange(j - 10, j);
-                    } else {
-                        result = nums[i] + sumRange(i + 1, j);
-                    }
-                    cache.put(currentMark, result);
-                }
-            }
 
-            return result;
+            if (i == 0) {
+                if (j == 0) {
+                    return nums[0];
+                }
+                return cache.get(new Marks(i, j));
+            } else {
+                Marks mark = new Marks(i, j);
+                Integer value = cache.get(mark);
+                if (value == null) {
+                    value = cache.get(new Marks(0, j)) - cache.get(new Marks(0, i - 1));
+                    cache.put(mark, value);
+                }
+                return value;
+            }
         }
     }
 
@@ -72,6 +91,9 @@ public class RangeSumQueryImmutable {
     }
 
     public static void main(String[] args) {
-
+        NumArray numArray = new NumArray(new int[]{1});
+//        System.out.println(numArray.sumRange(0, 0));
+//        System.out.println(numArray.sumRange(0, 1));
+//        System.out.println(numArray.sumRange(1, 1));
     }
 }
